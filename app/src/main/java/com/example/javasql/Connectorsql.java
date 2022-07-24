4c4c95ecb0f5d9c3edd888905e5e5e220698637c
unknown <mikechiave99@gmail.com>
2022-07-24 00:48:25 -0700

    firstcommit

---
+++ app/src/main/java/com/example/javasql/Connectorsql.java
@@ -0,0 +1,22 @@
+package com.example.javasql;
+
+import java.sql.Connection;
+
+public class Connectorsql {
+    Connection con;
+    String uname,pass,ip,port, database;
+    public Connection connectionclass(){
+        ip="127.0.0.1";
+        database= "newdb";
+        uname="root";
+        pass="Qpqpqp!!2";
+        port= "3306";
+        Connection connection=null;
+        String ConnectionUrl= null;
+        return connection;
+
+
+
+
+    }
+}
